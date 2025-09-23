ca43540976dc11e61c416b02d7aa3ad31cd8d8f5
Naman Urwar <Namanurwar092@gmail.com>
2025-09-23 12:14:13 +0530

    fix(utils): drop class-variance-authority; use clsx+tailwind-merge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 // src/lib/utils.ts
-import { type ClassValue } from "clsx";
-import { clsx } from "clsx";
+import type { ClassValue } from "clsx";
+import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
